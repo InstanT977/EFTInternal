3b8b9c92185cf82db4637e0e20619dd992f56a80
NoNameGod <47334467+NoNameG0d@users.noreply.github.com>
2020-06-08 14:38:24 +0100

    Update NamelessScreen.cs

--- Functions/NamelessScreen.cs
+++ Functions/NamelessScreen.cs
@@ -5,8 +5,8 @@ namespace EscapeFromGod.Functions
 {
     class NamelessScreen : MonoBehaviour
     {
-        public static float screenHeight = 1280f;
-        public static float screenWidth = 720f;
+        public static float screenHeight = 720f;
+        public static float screenWidth = 480f;
 
         private void Awake()
         {
